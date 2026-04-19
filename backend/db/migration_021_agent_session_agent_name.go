6360a2b8b47a69fa16fe0fe5b18e323fc3312d13
Li Zhao <iloahz@gmail.com>
2026-04-19 04:41:09 +0000

    db: add migration 021 renaming agent_file -> agent_name

---
+++ backend/db/migration_021_agent_session_agent_name.go
@@ -0,0 +1,31 @@
+package db
+
+import "database/sql"
+
+func init() {
+	RegisterMigration(Migration{
+		Version:     21,
+		Description: "Rename agent_sessions.agent_file to agent_name and strip .md suffix from existing values",
+		Up: func(db *sql.DB) error {
+			tx, err := db.Begin()
+			if err != nil {
+				return err
+			}
+			defer tx.Rollback()
+
+			if _, err := tx.Exec(`ALTER TABLE agent_sessions RENAME COLUMN agent_file TO agent_name`); err != nil {
+				return err
+			}
+
+			if _, err := tx.Exec(`
+				UPDATE agent_sessions
+				SET agent_name = substr(agent_name, 1, length(agent_name) - 3)
+				WHERE agent_name LIKE '%.md'
+			`); err != nil {
+				return err
+			}
+
+			return tx.Commit()
+		},
+	})
+}
